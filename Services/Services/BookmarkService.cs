df98fa301b5449cb95f7b22919dabf3be32bfe39
Rob Harlow <harlowhair@gmail.com>
2019-05-21 13:55:25 +0100

    Initial

---
+++ Services/Services/BookmarkService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReadLater.Entities;
+using ReadLater.Repository;
+
+namespace ReadLater.Services
+{
+    public class BookmarkService : IBookmarkService
+    {
+        protected IUnitOfWork _unitOfWork;
+
+        public BookmarkService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Bookmark CreateBookmark(Bookmark bookmark)
+        {
+            bookmark.CreateDate = DateTime.Now;
+            _unitOfWork.Repository<Bookmark>().Insert(bookmark);
+            _unitOfWork.Save();
+            return bookmark;
+        }
+
+        public List<Bookmark> GetBookmarks(string category)
+        {
+            if (string.IsNullOrEmpty(category))
+            {
+                return _unitOfWork.Repository<Bookmark>().Query()
+                                                        .OrderBy(l => l.OrderByDescending(b => b.CreateDate))
+                                                        .Get()                                                        
+                                                        .ToList();
+            }
+            else
+            {
+                return _unitOfWork.Repository<Bookmark>().Query()
+                                                            .Filter(b => b.Category != null && b.Category.Name == category)                                        
+                                                            .Get()
+                                                            .ToList();
+            }
+        }
+    }
+}
---
+++ Services/Services/IBookmarkService.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using ReadLater.Entities;
+
+namespace ReadLater.Services
+{
+    public interface IBookmarkService
+    {
+        Bookmark CreateBookmark(Bookmark bookmark);
+        List<Bookmark> GetBookmarks(string category);
+    }
+}
